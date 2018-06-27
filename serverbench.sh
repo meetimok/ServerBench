778cace0bee3527b019ba5cb041a7ddc11c2ca17
meetimok <ja.tavara@gmail.com>
2018-06-27 14:51:43 -0500

    alignment

--- serverbench.sh
+++ serverbench.sh
@@ -104,10 +104,10 @@ if  [ -e '/usr/bin/wget' ]; then
     echo "Average I/O speed    : $ioavg MB/s"
     next
 
-    printf "%-41s %16s %-11s\e[0m\n" "Node name" "IPv4 address" "Down. speed"
+    printf "%-41s %-16s %11s\e[0m\n" "Node name" "IPv4 address" "Down. speed"
     speed && next
     if [[ "$ipv6" != "" ]]; then
-        printf "%-41s %16s %-11s\e[0m\n" "Node name" "IPv6 address" "Down. speed"
+        printf "%-41s %-16s %11s\e[0m\n" "Node name" "IPv6 address" "Down. speed"
         speed_v6 && next
     fi
 else
